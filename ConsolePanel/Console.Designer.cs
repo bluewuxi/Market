7ec340c0be7ac46dd8d958b2d6af21dcaaa39f7d
ricky-PC\ricky <ricky.y@dataant.io>
2019-03-22 17:16:51 +1300

    init

---
+++ ConsolePanel/Console.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ConsolePanel
+{
+    partial class Console
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.buttonCaculate = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-3, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(802, 310);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonA
+            // 
+            this.buttonA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonA.Location = new System.Drawing.Point(19, 420);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(75, 51);
+            this.buttonA.TabIndex = 1;
+            this.buttonA.Text = "A";
+            this.buttonA.UseVisualStyleBackColor = true;
+            // 
+            // buttonB
+            // 
+            this.buttonB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonB.Location = new System.Drawing.Point(116, 420);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(75, 51);
+            this.buttonB.TabIndex = 2;
+            this.buttonB.Text = "B";
+            this.buttonB.UseVisualStyleBackColor = true;
+            // 
+            // buttonC
+            // 
+            this.buttonC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonC.Location = new System.Drawing.Point(212, 420);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(75, 51);
+            this.buttonC.TabIndex = 3;
+            this.buttonC.Text = "C";
+            this.buttonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonD
+            // 
+            this.buttonD.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonD.Location = new System.Drawing.Point(310, 420);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(75, 51);
+            this.buttonD.TabIndex = 4;
+            this.buttonD.Text = "D";
+            this.buttonD.UseVisualStyleBackColor = true;
+            // 
+            // buttonCaculate
+            // 
+            this.buttonCaculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonCaculate.Location = new System.Drawing.Point(438, 420);
+            this.buttonCaculate.Name = "buttonCaculate";
+            this.buttonCaculate.Size = new System.Drawing.Size(137, 51);
+            this.buttonCaculate.TabIndex = 5;
+            this.buttonCaculate.Text = "Caculate";
+            this.buttonCaculate.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(590, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "AC";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Console
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 606);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonCaculate);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonA);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Console";
+            this.Text = "Console";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.Button buttonCaculate;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Market/PointOfSaleTerminal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MarketLib
+{
+    public class PointOfSaleTerminal
+    {
+        public void SetPricing()
+        {
+
+        }
+
+        public void ScanProduct(string code)
+        {
+
+        }
+
+        public decimal CalculateTotal()
+        {
+            return 0;
+        }
+    }
+}
